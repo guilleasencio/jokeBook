0deafce8c5bbd002d089ef02d5870d4f90227081
Guillermo Asencio Sanchez <guillermo.asencio.sanchez@gmail.com>
2021-02-28 19:06:45 +0100

    Add NetworkManager

---
+++ jokeBookApp/jokeBookApp/Managers/Managers.swift
@@ -0,0 +1,15 @@
+//
+//  Managers.swift
+//  jokeBookApp
+//
+//  Created by Guillermo Asencio Sanchez on 28/2/21.
+//
+
+import Foundation
+
+class Managers {
+  
+  // MARK: - Properties
+  
+  static let network = NetworkManager()
+}
